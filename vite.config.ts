d11c11c599cd7cfca7499ab367cdc8922ddc1b1c
Josh Manning <josh@jman.me>
2025-03-30 12:27:58 -0700

    fix web build

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import {TanStackRouterVite} from '@tanstack/router-plugin/vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
-export default defineConfig({plugins: [TanStackRouterVite({}), react()]})
+export default defineConfig({plugins: [tsconfigPaths(), TanStackRouterVite({}), react()]})
